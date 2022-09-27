65b78fc2390994405fb6377e9fbce4633ecd0db6
Jakub Vojtek <vojtekjakub1@gmail.com>
2022-09-27 22:40:02 +0200

    Fixed bugs in code

--- hanoi.c
+++ hanoi.c
@@ -6,9 +6,9 @@ void hanoi(int n, int a, int b)
     printf("move stone 1 from %d to %d\n", a, b);
   else
   {
-    hanoi(n - 1, a, 6 - a);
+    hanoi(n - 1, a, 6 - a-b);
     printf("move stone %d from %d to %d\n", n, a, b);
-    hanoi(n, 6 - a - b, b);
+    hanoi(n-1, 6 - a - b, b);
   }
 }
 
