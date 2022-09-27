0f92ff52e7ae2967e62b55b79b2037baca2c089f
1Andrea0 <andreazalobinova9@gmail.com>
2022-09-27 22:49:40 +0200

    Translate to Slovak

--- hanoi.c
+++ hanoi.c
@@ -3,11 +3,11 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("move stone 1 from %d to %d\n", a, b);
+    printf("kamen 1 zos %d dos %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a-b);
-    printf("move stone %d from %d to %d\n", n, a, b);
+    printf("kamen %d zos %d dos %d\n", n, a, b);
     hanoi(n-1, 6 - a - b, b);
   }
 }
